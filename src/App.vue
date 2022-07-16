383b0d0b21e8acc348dadd49169a9cb61f421c7b
Pedro Diogo <pedrodiogo219@gmail.com>
2022-07-16 14:20:17 -0300

    add id to noticia obj

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,7 @@ export default{
   
   computed: {
     currentView() {
+      console.log(this.currentPath)
       return routes[this.currentPath.slice(1) || '/'] || NotFound
     }
   },
@@ -59,6 +60,7 @@ export default{
   methods: {
     chama: function(){
       this.noticias.value.push({
+        id: 1,
         titulo: 'Primeira Noticia',
         descricao: 'Essa é a primeira noticia do site',
         corpo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem nisi, volutpat vel libero eget, bibendum pellentesque nisi. Ut a laoreet libero. Vestibulum euismod ipsum sed dictum semper. Sed non ante non arcu interdum commodo. Morbi ultricies, elit id mattis porttitor, diam lectus rutrum lectus, non laoreet orci velit ac nisi. Sed vel diam feugiat, dictum dolor vitae, cursus orci. Quisque eget nunc porttitor, euismod libero et, pretium velit. Cras mauris est, bibendum in feugiat tincidunt, aliquet a sem. Nullam ac nisl dignissim felis maximus euismod non vel ipsum. Aliquam imperdiet interdum lacus, sit amet dictum tortor tristique sed. Fusce hendrerit mattis dui quis congue. Fusce orci orci, placerat consectetur sem id, consequat tempus ipsum. Nullam vitae risus lectus. Sed consequat aliquet velit eget semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
