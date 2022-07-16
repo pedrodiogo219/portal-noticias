a46b8c7d24760f1f06e1e6e46fe546aded13db4d
Pedro Diogo <pedrodiogo219@gmail.com>
2022-07-16 10:54:10 -0300

    add noticiaCompleta component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <NoticiaCompleta></NoticiaCompleta>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NoticiaCompleta from './components/NoticiaCompleta.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NoticiaCompleta
   }
 }
 </script>
