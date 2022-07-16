908f30a9abf428d17e871748d4c7730e860630a6
Pedro Diogo <pedrodiogo219@gmail.com>
2022-07-16 13:52:37 -0300

    implementar feed e resumo noticia

--- src/components/FeedNoticias.vue
+++ src/components/FeedNoticias.vue
@@ -2,21 +2,36 @@
     <h1>feed</h1>
     <button v-on:click="mostra()">mostra</button>
 
-    <ul>
-        <li v-for="(noticia, index) in this.$parent.noticias.value" :key="index">{{noticia.titulo}}</li>
-    </ul>
+    <div id="listaNoticias">
+        <ResumoNoticia v-for="(noticia, index) in this.$parent.noticias.value" :key="index" :noticia="noticia"></ResumoNoticia>
+    </div>
 
 </template>
 
 <script>
+    import ResumoNoticia from './ResumoNoticia.vue';
+
+
     export default {
-        created(){
-            console.log(this.$parent.noticias)
-        },
-        methods: {
-            mostra: function(){
-                console.log(this.$parent.noticias)
-            }
+    created() {
+        console.log(this.$parent.noticias);
+    },
+    methods: {
+        mostra: function () {
+            console.log(this.$parent.noticias);
         }
-    }
+    },
+    components: { ResumoNoticia }
+}
 </script>
+
+
+<style scoped>
+
+#listaNoticias{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+</style>
--- src/components/ResumoNoticia.vue
+++ src/components/ResumoNoticia.vue
@@ -1,1 +1,20 @@
-<template><h3>resumo noticia</h3></template>
+<template>
+    <div id="container">
+        <h3>{{ noticia.titulo }}</h3>
+        <p>{{ noticia.descricao }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['noticia']
+}
+</script>
+
+<style scoped>
+#container{
+    border: 1px solid black;
+    display: block;
+}
+</style>
