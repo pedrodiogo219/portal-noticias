b69f3b0adf6c22a7f5db57bf2b0d0a5a24145c40
Pedro Diogo <pedrodiogo219@gmail.com>
2022-07-25 11:01:26 -0300

    fix url nova-noticia

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
     
                 
                 <a href="/">Feed</a>
-                <a href="nova-noticia">Nova noticia</a>
+                <a href="/nova-noticia">Nova noticia</a>
             </div>
             
             <!-- <a href="#contact" :disabled='true' >Contact</a> -->
